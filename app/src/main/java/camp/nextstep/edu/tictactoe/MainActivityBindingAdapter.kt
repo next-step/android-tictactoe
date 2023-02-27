6b3f881bae3d17bb8255b23f567539dd75f48523
James <bookwriterseo@gmail.com>
2023-02-27 17:27:19 +0900

    feat: 셀을 선택했을 때 X와 O 번갈아 표시하기

---
+++ app/src/main/java/camp/nextstep/edu/tictactoe/MainActivityBindingAdapter.kt
@@ -0,0 +1,19 @@
+package camp.nextstep.edu.tictactoe
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("src")
+fun setImageViewResource(imageView: ImageView, isFirst: Boolean?) {
+    when (isFirst) {
+        true -> {
+            imageView.setImageResource(R.drawable.ic_x_black)
+        }
+        false -> {
+            imageView.setImageResource(R.drawable.ic_o_black)
+        }
+        else -> {
+            imageView.setImageResource(R.color.transparent)
+        }
+    }
+}
--- app/src/main/java/camp/nextstep/edu/tictactoe/TictactoeViewModel.kt
+++ app/src/main/java/camp/nextstep/edu/tictactoe/TictactoeViewModel.kt
@@ -1,6 +1,34 @@
 package camp.nextstep.edu.tictactoe
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TictactoeViewModel : ViewModel() {
+    private val _isFirst: MutableLiveData<Boolean?> = MutableLiveData(null)
+    val isFirst: LiveData<Boolean?> = _isFirst
+
+    private val _board: MutableList<MutableLiveData<Boolean?>> =
+        MutableList(BOARD_SIZE) { MutableLiveData<Boolean?>(null) }
+    val board: List<LiveData<Boolean?>>
+        get() = _board.toList()
+
+    fun mark(position: Int) {
+        if (_board[position].value == null) {
+            setIsFirstValue()
+            _board[position].value = _isFirst.value
+        }
+    }
+
+    private fun setIsFirstValue() {
+        if (_isFirst.value == null) {
+            _isFirst.value = true
+        } else {
+            _isFirst.value = _isFirst.value != true
+        }
+    }
+
+    companion object {
+        private const val BOARD_SIZE = 9
+    }
 }
