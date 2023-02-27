229d48d2912437f5d6a48cf98ef6266100c202b0
jpoh281 <hjp4869@naver.com>
2023-02-27 22:20:05 +0900

    feat: equals, hashCode 구현

--- domain/src/main/java/com/example/domain/Block.kt
+++ domain/src/main/java/com/example/domain/Block.kt
@@ -13,6 +13,16 @@ class EmptyBlock : Block() {
     override fun toString(): String {
         return "EmptyBlock()"
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
 }
 
 sealed class AssignedBlock : Block()
