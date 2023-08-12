229d5e840d68223601e582bd071c046a7c8b93d9
duru.hi <hwanggyuil@gmail.com>
2023-08-13 00:11:34 +0900

    feat. ViewModel 재시작 테스트 케이스 추가

--- app/src/test/java/camp/nextstep/edu/tictactoe/TictactocViewModelTest.kt
+++ app/src/test/java/camp/nextstep/edu/tictactoe/TictactocViewModelTest.kt
@@ -219,6 +219,43 @@ class TictactocViewModelTest {
         assertEquals(TictactocToastMessage.GameOver, actual)
     }
 
+    @Test
+    fun `결판이_안나서_재시작_후_가로_한줄을_x가_먼저_채우면_x가_승리한다`() {
+        // given: 아래와 같이 결판이 안난다.
+        /**
+         * x x o
+         * o o x
+         * x o x
+         **/
+        onSetBoardPoint(Point.CellTopLeft)
+        onSetBoardPoint(Point.CellTopRight)
+        onSetBoardPoint(Point.CellTop)
+        onSetBoardPoint(Point.CellMiddleLeft)
+        onSetBoardPoint(Point.CellMiddleRight)
+        onSetBoardPoint(Point.CellMiddle)
+        onSetBoardPoint(Point.CellBottomLeft)
+        onSetBoardPoint(Point.CellBottom)
+        onSetBoardPoint(Point.CellBottomRight)
+
+        // when: 재시작을 누르고 아래와 같이 x가 가로 한줄을 먼저 채운다.
+        /**
+         * x x x
+         * o o
+         *
+         **/
+        viewModel.onRestBoard()
+
+        onSetBoardPoint(Point.CellTopLeft)
+        onSetBoardPoint(Point.CellMiddleLeft)
+        onSetBoardPoint(Point.CellTop)
+        onSetBoardPoint(Point.CellMiddle)
+        onSetBoardPoint(Point.CellTopRight)
+
+        // then: x가 승리한다.
+        val actual = viewModel.tictactocToastMessage.getOrAwaitValue()
+        assertEquals(TictactocToastMessage.XWin, actual)
+    }
+
     private fun onSetBoardPoint(point: Point) {
         viewModel.onSetBoardPoint(point)
     }
