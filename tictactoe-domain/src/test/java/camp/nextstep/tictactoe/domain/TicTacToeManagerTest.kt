5b055f7c503cf005aaad08456ac39ea5efae4f3a
김영욱 <0.uk.kim@navercorp.com>
2023-08-17 23:19:56 +0900

    틱택토 게임 상태 반환 로직 테스트 코드 작성

--- tictactoe-domain/src/test/java/camp/nextstep/tictactoe/domain/TicTacToeManagerTest.kt
+++ tictactoe-domain/src/test/java/camp/nextstep/tictactoe/domain/TicTacToeManagerTest.kt
@@ -21,4 +21,96 @@ class TicTacToeManagerTest {
 		// then
 		assertThat(actual).isEqualTo(Board(map = mapOf(Point(0, 0) to Marker.X)))
 	}
+
+	@Test
+	fun `행 한줄이 X 표시로 채워졌을 때, 게임 상태를 가져오면, 게임 종료 상태를 반환한다`() {
+		// given
+		val board = Board(
+			map = mapOf(
+				Point(0, 0) to Marker.X,
+				Point(0, 1) to Marker.X,
+				Point(0, 2) to Marker.X,
+			))
+
+		// when
+		val actual = ticTacToeManager.getGameStatus(board)
+
+		// then
+		assertThat(actual).isEqualTo(GameStatus.End(Marker.X))
+	}
+
+	@Test
+	fun `열 한줄이 X 표시로 채워졌을 때, 게임 상태를 가져오면, 게임 종료 상태를 반환한다`() {
+		// given
+		val board = Board(
+			map = mapOf(
+				Point(0, 0) to Marker.X,
+				Point(1, 0) to Marker.X,
+				Point(2, 0) to Marker.X,
+			))
+
+		// when
+		val actual = ticTacToeManager.getGameStatus(board)
+
+		// then
+		assertThat(actual).isEqualTo(GameStatus.End(Marker.X))
+	}
+
+	@Test
+	fun `대각선 한줄이 X 표시로 채워졌을 때, 게임 상태를 가져오면, 게임 종료 상태를 반환한다`() {
+		// given
+		val board = Board(
+			map = mapOf(
+				Point(0, 0) to Marker.X,
+				Point(1, 1) to Marker.X,
+				Point(2, 2) to Marker.X,
+			))
+
+		// when
+		val actual = ticTacToeManager.getGameStatus(board)
+
+		// then
+		assertThat(actual).isEqualTo(GameStatus.End(Marker.X))
+	}
+
+	@Test
+	fun `어느 한줄도 X,O 표시로 채워지지 않았을 때, 게임 상태를 가져오면, 진행중 상태를 반환한다`() {
+		// given
+		val board = Board(
+			map = mapOf(
+				Point(0, 0) to Marker.X,
+				Point(0, 1) to Marker.O,
+				Point(1, 0) to Marker.X,
+				Point(1, 1) to Marker.O,
+			))
+
+		// when
+		val actual = ticTacToeManager.getGameStatus(board)
+
+		// then
+		assertThat(actual).isEqualTo(GameStatus.InProgress)
+	}
+
+	@Test
+	fun `모든 줄이 X,O 표시로 채워졌을 때, 게임 상태를 가져오면, 무승부 상태를 반환한다`() {
+		// given
+		val board = Board(
+			map = mapOf(
+				Point(0, 0) to Marker.X,
+				Point(0, 1) to Marker.O,
+				Point(0, 2) to Marker.X,
+				Point(1, 0) to Marker.O,
+				Point(1, 1) to Marker.X,
+				Point(1, 2) to Marker.X,
+				Point(2, 0) to Marker.O,
+				Point(2, 1) to Marker.X,
+				Point(2, 2) to Marker.O,
+			))
+
+		// when
+		val actual = ticTacToeManager.getGameStatus(board)
+
+		// then
+		assertThat(actual).isEqualTo(GameStatus.Draw)
+	}
 }
