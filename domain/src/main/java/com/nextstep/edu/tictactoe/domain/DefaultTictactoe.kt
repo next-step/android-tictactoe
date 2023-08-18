d25f3671f99025b1e10367776add4c5a108895b6
duru.hi <hwanggyuil@gmail.com>
2023-08-18 18:53:00 +0900

    fix. step2 피드백 반영중(put 메서드 인터페이스화 <- DefaultTictactoc 함수 정리)

--- domain/src/main/java/com/nextstep/edu/tictactoe/domain/DefaultTictactoe.kt
+++ domain/src/main/java/com/nextstep/edu/tictactoe/domain/DefaultTictactoe.kt
@@ -8,31 +8,18 @@ import com.nextstep.edu.tictactoe.domain.model.Turn
 abstract class DefaultTictactoe: TictactocPut {
 
     protected val tictactocMap: TictactocMap = TictactocMap()
-    private var currentTurn: Turn = Turn.X
 
-    fun getMap(): Array<Array<Turn>> {
-        return tictactocMap.getMap()
-    }
+    fun getMap(): Array<Array<Turn>> = tictactocMap.getMap()
 
-    fun getCurrentTurn(): Turn {
-        return tictactocMap.getCurrentTurn()
-    }
+    fun getCurrentTurn(): Turn = tictactocMap.getCurrentTurn()
 
-    protected fun isValidData(point: Point): Boolean {
-        return tictactocMap.validData(point = point)
-    }
+    protected fun isValidData(point: Point): Boolean = tictactocMap.validData(point = point)
 
-    protected fun getGameResult(point: Point): GameResult {
-        return tictactocMap.getGameResultFromSetMapPoint(point = point)
-    }
+    protected fun getGameResult(point: Point): GameResult = tictactocMap.getGameResultFromSetMapPoint(point = point)
 
-    fun reset() {
-        tictactocMap.resetMap()
-    }
+    fun reset() = tictactocMap.resetMap()
 
-    fun changeTurn() {
-        tictactocMap.changeTurn()
-    }
+    fun changeTurn() = tictactocMap.changeTurn()
 
     companion object {
         const val MAP_SIZE = 3
