f3de97bdacef8166ebb0dfabb07c45f4ddaf3422
김영욱 <0.uk.kim@navercorp.com>
2023-08-22 18:27:20 +0900

    #step2 : 랜덤 AI 플레이어 추가

--- tictactoe-domain/src/main/java/camp/nextstep/tictactoe/domain/Player.kt
+++ tictactoe-domain/src/main/java/camp/nextstep/tictactoe/domain/Player.kt
@@ -4,4 +4,9 @@ sealed interface Player {
 	val marker: Marker
 
 	data class Person(override val marker: Marker) : Player
+	data class Ai(override val marker: Marker, val level: Level): Player
+}
+
+enum class Level {
+	Random
 }
