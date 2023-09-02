745959e8a6a2e47d5a600dd8a378892915440c35
OYJ <oyj7677@gmail.lcom>
2023-09-02 20:01:28 +0900

    뷰모델 유닛테스트 수정

--- app/src/test/java/camp/nextstep/edu/tictactoe/TictactoeViewModelTest.kt
+++ app/src/test/java/camp/nextstep/edu/tictactoe/TictactoeViewModelTest.kt
@@ -2,6 +2,7 @@ package camp.nextstep.edu.tictactoe
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.tictectoe_domain.Cell
+import com.example.tictectoe_domain.Game
 import com.example.tictectoe_domain.TictectoeBoard
 import com.example.tictectoe_domain.TictectoeRule
 import com.google.common.truth.Truth.assertThat
@@ -13,15 +14,11 @@ class TictactoeViewModelTest {
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var gameBoard: TictectoeBoard
-    private lateinit var gameRule: TictectoeRule
     private lateinit var viewModel: TictactoeViewModel
 
     @Before
     fun setTest() {
-        gameBoard = TictectoeBoard()
-        gameRule = TictectoeRule()
-        viewModel = TictactoeViewModel(gameBoard, gameRule)
+        viewModel = TictactoeViewModel(Game())
     }
 
     @Test
@@ -32,7 +29,7 @@ class TictactoeViewModelTest {
         viewModel.clickBoard(1)
 
         // then : 보드의 해당 위치에 플레이어가 등록된다.
-        assertThat(gameBoard.getBoard()[1]).isEqualTo(Cell.PLAYER1)
+        assertThat(viewModel.board).isEqualTo(Cell.PLAYER1)
     }
 
     @Test
@@ -43,19 +40,7 @@ class TictactoeViewModelTest {
         viewModel.clickBoard(1)
         viewModel.clickBoard(2)
 
-        // then : 보드의 해당 위치에 플레이어가 등록된다.
-        assertThat(gameBoard.getBoard()[2]).isEqualTo(Cell.PLAYER2)
-    }
-
-    @Test
-    fun `보드를 선택하면 Player가 변경된다`() {
-        // given : 기존 플레이어를 확인한다.
-        assertThat(gameBoard.getPlayer()).isEqualTo(Cell.PLAYER1)
-
-        // when : 보드를 한번 클릭한다.
-        viewModel.clickBoard(1)
-
-        // then : 변경된 플레이어를 확인한다
-        assertThat(gameBoard.getPlayer()).isEqualTo(Cell.PLAYER2)
+        // then : 보드의 2번 셀에 PLAYER2가 등록된다.
+        assertThat(viewModel.board.value!![2]).isEqualTo(Cell.PLAYER2)
     }
 }
