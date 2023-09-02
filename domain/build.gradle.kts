cd3022e2c1e854cdf14b9a90865181e518ea35cc
KwonDae <green201402317@gmail.com>
2023-09-02 12:58:50 +0900

    domain - 테스트 디펜던시 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -6,3 +6,9 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.google.truth:truth:1.1.5")
+    testImplementation("io.mockk:mockk:1.13.7")
+}
