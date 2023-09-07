9c44f331d08df4342f9c1b13c715e0fdfad8f4d5
OYJ <oyj7677@gmail.lcom>
2023-09-08 01:04:31 +0900

    테스트 케이스 수정  - 무승부 보드 추가

--- tictectoe-domain/src/test/kotlin/com/example/tictectoe_domain/TictectoeRuleTest.kt
+++ tictectoe-domain/src/test/kotlin/com/example/tictectoe_domain/TictectoeRuleTest.kt
@@ -16,133 +16,24 @@ class TictectoeRuleTest {
     @Test
     fun `상단 가로줄 모두 player1이 차지할 경우 승리한 자는 player1이다`() {
         // given : 상단 가로줄 모두 player1인 보드를 만든다.
-
-        val board = Board.TEST_BOARD
+        val board = Board.TEST_PLAYER1_WIN_BOARD
 
         // when : 게임에 승리 조건에 보드를 넣는다.
         val actual = gameRule.checkGameStatus(board)
 
-        // then : 게임상태는 GameStatus.PLAYER1_WIN이다.
+        // then : 게임 상태는 GameStatus.PLAYER1_WIN을 반환한다.
         assertThat(actual).isEqualTo(GameStatus.PLAYER1_WIN)
     }
 
-//    @Test
-//    fun `0번 인덱스를 제외하고 빈 공간이 0개라면 무승부이다`() {
-//        // given : 승리 조건을 만족하지 않고 빈 공간이 1개인 보드를 만든다.
-//        val board = mutableListOf(Cell.NONE,
-//            Cell.PLAYER2, Cell.PLAYER1, Cell.PLAYER1,
-//            Cell.PLAYER1, Cell.PLAYER2, Cell.PLAYER2,
-//            Cell.PLAYER2, Cell.PLAYER1, Cell.PLAYER1,)
-//
-//        // when : 게임에 승리 조건에 보드를 넣는다.
-//        val actual = gameRule.getWinningPlayer(board)
-//
-//        // then : None이 반환된다.
-//        assertThat(actual).isEqualTo(Cell.NONE)
-//    }
-//
-//    @Test
-//    fun `중단 가로줄 모두 player1이 차지할 경우 승리한 자는 player1이다`() {
-//        // given : 중단 가로줄 모두 player1인 보드를 만든다.
-//        val board = mutableListOf(Cell.NONE,
-//            Cell.NONE, Cell.NONE, Cell.NONE,
-//            Cell.PLAYER1, Cell.PLAYER1, Cell.PLAYER1,
-//            Cell.NONE, Cell.NONE, Cell.NONE,)
-//
-//        // when : 게임에 승리 조건에 보드를 넣는다.
-//        val actual = gameRule.getWinningPlayer(board)
-//
-//        // then : Player1이 반환된다.
-//        assertThat(actual).isEqualTo(Cell.PLAYER1)
-//    }
-//
-//    @Test
-//    fun `하단 가로줄 모두 player1이 차지할 경우 승리한 자는 player1이다`() {
-//        // given : 하단 가로줄 모두 player1인 보드를 만든다.
-//        val board = mutableListOf(Cell.NONE,
-//            Cell.NONE, Cell.NONE, Cell.NONE,
-//            Cell.NONE, Cell.NONE, Cell.NONE,
-//            Cell.PLAYER1, Cell.PLAYER1, Cell.PLAYER1)
-//
-//        // when : 게임에 승리 조건에 보드를 넣는다.
-//        val actual = gameRule.getWinningPlayer(board)
-//
-//        // then : Player1이 반환된다.
-//        assertThat(actual).isEqualTo(Cell.PLAYER1)
-//    }
-//
-//    @Test
-//    fun `좌측 세로줄 모두 player2가 차지할 경우 승리한 자는 player2이다`() {
-//        // given : 좌측 세로줄 모두 player2인 보드를 만든다.
-//        val board = mutableListOf(Cell.NONE,
-//            Cell.PLAYER2, Cell.NONE, Cell.NONE,
-//            Cell.PLAYER2, Cell.NONE, Cell.NONE,
-//            Cell.PLAYER2, Cell.NONE, Cell.NONE,)
-//
-//        // when : 게임에 승리 조건에 보드를 넣는다.
-//        val actual = gameRule.getWinningPlayer(board)
-//
-//        // then : Player2이 반환된다.
-//        assertThat(actual).isEqualTo(Cell.PLAYER2)
-//    }
-//
-//    @Test
-//    fun `중앙 세로줄 모두 player2가 차지할 경우 승리한 자는 player2이다`() {
-//        // given : 중앙 세로줄 모두 player2인 보드를 만든다.
-//        val board = mutableListOf(Cell.NONE,
-//            Cell.NONE, Cell.PLAYER2, Cell.NONE,
-//            Cell.NONE, Cell.PLAYER2, Cell.NONE,
-//            Cell.NONE, Cell.PLAYER2, Cell.NONE,)
-//
-//        // when : 게임에 승리 조건에 보드를 넣는다.
-//        val actual = gameRule.getWinningPlayer(board)
-//
-//        // then : Player2이 반환된다.
-//        assertThat(actual).isEqualTo(Cell.PLAYER2)
-//    }
-//
-//    @Test
-//    fun `우측 세로줄 모두 player2가 차지할 경우 승리한 자는 player2이다`() {
-//        // given : 우측이 모두 player2인 보드를 만든다.
-//        val board = mutableListOf(Cell.NONE,
-//            Cell.NONE, Cell.NONE, Cell.PLAYER2,
-//            Cell.NONE, Cell.NONE, Cell.PLAYER2,
-//            Cell.NONE, Cell.NONE, Cell.PLAYER2,)
-//
-//        // when : 게임에 승리 조건에 보드를 넣는다.
-//        val actual = gameRule.getWinningPlayer(board)
-//
-//        // then : Player2이 반환된다.
-//        assertThat(actual).isEqualTo(Cell.PLAYER2)
-//    }
-//
-//    @Test
-//    fun `우측 상단에서 시작하의 대각선을 모두 player2가 차지할 경우 승리한 자는 player2이다`() {
-//        // given : 우측 상단에서 시작하는 대각선이 모두 player2인 보드를 만든다.
-//        val board = mutableListOf(Cell.NONE,
-//            Cell.PLAYER2, Cell.NONE, Cell.NONE,
-//            Cell.NONE, Cell.PLAYER2, Cell.NONE,
-//            Cell.NONE, Cell.NONE, Cell.PLAYER2,)
-//
-//        // when : 게임에 승리 조건에 보드를 넣는다.
-//        val actual = gameRule.getWinningPlayer(board)
-//
-//        // then : Player2이 반환된다.
-//        assertThat(actual).isEqualTo(Cell.PLAYER2)
-//    }
-//
-//    @Test
-//    fun `좌측 상단에서 시작하의 대각선을 모두 player2가 차지할 경우 승리한 자는 player2이다`() {
-//        // given : 좌측 상단에서 시작하는 대각선이 모두 player2인 보드를 만든다.
-//        val board = mutableListOf(Cell.NONE,
-//            Cell.NONE, Cell.NONE, Cell.PLAYER2,
-//            Cell.NONE, Cell.PLAYER2, Cell.NONE,
-//            Cell.PLAYER2, Cell.NONE, Cell.NONE,)
-//
-//        // when : 게임에 승리 조건에 보드를 넣는다.
-//        val actual = gameRule.getWinningPlayer(board)
-//
-//        // then : Player2이 반환된다.
-//        assertThat(actual).isEqualTo(Cell.PLAYER2)
-//    }
+    @Test
+    fun `모든 셀이 채워져도 승리하는 조건을 만족하지 않으면 무승부다`() {
+        // given : 승리 조건을 만족하지 않는 보드를 만든다.
+        val board = Board.TEST_DRAW_BOARD
+
+        // when : 게임에 승리 조건에 보드를 넣는다.
+        val actual = gameRule.checkGameStatus(board)
+
+        // then : 게임 상태는  GameStatus.DRAW_GAME를 반환한다.
+        assertThat(actual).isEqualTo(GameStatus.DRAW_GAME)
+    }
 }
